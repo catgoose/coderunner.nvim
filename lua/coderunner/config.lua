d36fec722b4d342660f4fd33bacd9b906b79902c
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-03-09 05:25:16 -0600

    setting command by tables

--- lua/coderunner/config.lua
+++ lua/coderunner/config.lua
@@ -3,46 +3,30 @@ local M = {
 		dev = false,
 		langs = {
 			ruby = {
-				cmd = "ruby",
-				-- cmd_args = "-w",
-				cmd_args = "",
-				args = "",
+				cmd = { "ruby" },
 			},
 			lua = {
-				cmd = "lua",
-				cmd_args = "",
-				args = "",
+				cmd = { "lua" },
 			},
 			javascript = {
-				cmd = "node",
-				cmd_args = "",
-				args = "",
+				cmd = { "node" },
 			},
 			python = {
-				cmd = "python3",
-				cmd_args = "",
-				args = "",
+				cmd = { "python3" },
+			},
+			cpp = {
+				"make",
 			},
 		},
 		split = "horizontal",
 		scale = 0.25,
 		filetype = "coderunner",
-		cursor = {
-			lastline = true,
-		},
 	},
 }
-
-local dev = function()
-	require("coderunner.utils").dev()
-end
 
 M.init = function(opts)
 	opts = opts or {}
 	M.opts = vim.tbl_deep_extend("keep", opts, M.opts)
-	if M.opts.dev then
-		dev()
-	end
 	return M.opts
 end
 
