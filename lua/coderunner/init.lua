0c904f40ed49adb02903dcd121ba7ba4b0ee5d0d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-12-16 06:31:20 -0600

    feat: creates user commands

--- lua/coderunner/init.lua
+++ lua/coderunner/init.lua
@@ -5,6 +5,15 @@ local M = {}
 M.setup = function(config)
 	config = config or {}
 	cnf.init(config)
+	vim.api.nvim_create_user_command("Coderunner", function()
+		M.run()
+	end, {})
+	vim.api.nvim_create_user_command("CoderunnerHorizontal", function()
+		M.run({ split = "horizontal" })
+	end, {})
+	vim.api.nvim_create_user_command("CoderunnerVertical", function()
+		M.run({ split = "vertical" })
+	end, {})
 end
 
 M.run = function(config)
